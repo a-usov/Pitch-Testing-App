d5cbbd6b7c06574d212066f68aa2ea1fb4f30bd2
Olga Jodelka <jool97@gmail.com>
2018-11-25 01:43:53 +0000

    Added dropdown menu, issue #37 almost done (no action bar)

--- SoundRecord/app/src/main/java/com/example/conal/soundrecord/Home.java
+++ SoundRecord/app/src/main/java/com/example/conal/soundrecord/Home.java
@@ -3,8 +3,11 @@ package com.example.conal.soundrecord;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
@@ -54,4 +57,35 @@ public class Home extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void openHomePage(){
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+
+    public void openMyTestsPage(){
+        Intent intent = new Intent(this, MyTests.class);
+        startActivity(intent);
+    }
+
+    //dropdown menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.home) {
+            openHomePage();
+        }
+        else if(item.getItemId() == R.id.my_tests){
+            openMyTestsPage();
+        }else{
+            Toast.makeText(this, "This will be My Acoount page", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- SoundRecord/app/src/main/java/com/example/conal/soundrecord/MyTests.java
+++ SoundRecord/app/src/main/java/com/example/conal/soundrecord/MyTests.java
@@ -1,11 +1,11 @@
 package com.example.conal.soundrecord;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MyTests extends AppCompatActivity {
 
@@ -13,17 +13,37 @@ public class MyTests extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_tests);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+    public void openHomePage(){
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+
+    public void openMyTestsPage(){
+        Intent intent = new Intent(this, MyTests.class);
+        startActivity(intent);
+    }
+
+    //dropdown menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.home){
+            openHomePage();
+        }else if(item.getItemId() == R.id.my_tests){
+            openMyTestsPage();
+        }else{
+            Toast.makeText(this, "This will be My Acoount page", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
