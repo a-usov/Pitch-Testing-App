a2a508e7f64d00f891a5b9256a7a258c6463f934
Conall Clements <2197397c@student.gla.ac.uk>
2018-12-04 15:14:28 +0000

    Merged Main and Maps activities

--- SoundRecord/app/src/main/java/com/example/conal/soundrecord/MapsActivity.java
+++ SoundRecord/app/src/main/java/com/example/conal/soundrecord/MapsActivity.java
@@ -1,8 +1,22 @@
 package com.example.conal.soundrecord;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,8 +27,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.IOException;
+import java.util.UUID;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    final int REQUEST_PERMISSION_CODE = 1000;
+    //Declare variables
+    Button btnStartRecord, btnStopRecord;
+    String pathSave = "";
+    MediaRecorder mediaRecorder;
     private GoogleMap mMap;
 
     @Override
@@ -37,8 +59,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
+
+
+        if (!checkPermissionFromDevice()) requestPermission();
+
+        btnStopRecord = this.<Button>findViewById(R.id.btnStopRecord);
+        btnStartRecord = this.<Button>findViewById(R.id.btnStartRecord);
+
+        btnStartRecord.setEnabled(true);
+        btnStopRecord.setEnabled(false);
+
+
+        btnStartRecord.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (checkPermissionFromDevice()) {
+                    pathSave = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
+                            UUID.randomUUID().toString() + "_audio_record.3_gpp";
+                    setupMediaRecorder();
+
+                    try {
+                        mediaRecorder.prepare();
+                        mediaRecorder.start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    btnStartRecord.setEnabled(false);
+                    btnStopRecord.setEnabled(true);
+
+                    Toast.makeText(MapsActivity.this, "Recording...", Toast.LENGTH_SHORT).show();
+                } else {
+                    requestPermission();
+                }
+            }
+        });
+
+        btnStopRecord.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mediaRecorder.stop();
+                btnStartRecord.setEnabled(true);
+                btnStopRecord.setEnabled(false);
+
+                openProcessingActivity();
+            }
+        });
+        }
 
 
     /**
@@ -58,5 +123,80 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void setupMediaRecorder() {
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        mediaRecorder.setOutputFile(pathSave);
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.RECORD_AUDIO
+        }, REQUEST_PERMISSION_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+            break;
+
+        }
+    }
+
+    private boolean checkPermissionFromDevice() {
+        int write_external_storage_result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int record_audio_result = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        return write_external_storage_result == PackageManager.PERMISSION_GRANTED &&
+                record_audio_result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void openProcessingActivity(){
+        Intent intent = new Intent(this, Processing.class);
+        startActivity(intent);
+    }
+
+    public void openHomePage(){
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+
+    public void openMyTestsPage(){
+        Intent intent = new Intent(this, MyTests.class);
+        startActivity(intent);
+    }
+
+    //dropdown menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.home) {
+            openHomePage();
+        }
+        else if(item.getItemId() == R.id.my_tests){
+            openMyTestsPage();
+        }else{
+            Toast.makeText(this, "This will be My Account page", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
