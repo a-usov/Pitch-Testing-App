6d84a527d46ce5954e4294f526c49bc438ecc2a6
Conall Clements <2197397c@student.gla.ac.uk>
2019-02-06 15:36:03 +0000

    Update SoundRecord/app/src/main/java/com/example/conal/soundrecord/MyTests.java

--- SoundRecord/app/src/main/java/com/example/conal/soundrecord/MyTests.java
+++ SoundRecord/app/src/main/java/com/example/conal/soundrecord/MyTests.java
@@ -1,7 +1,9 @@
 package com.example.conal.soundrecord;
 
 import android.content.Context;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
