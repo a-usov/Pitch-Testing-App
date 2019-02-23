a7106cccb0774547e631280543e10e5c280f3cd1
Kara <karajanenewlands@gmail.com>
2019-02-23 15:01:57 +0000

    Adding missing functionality from results page back in

--- SoundRecord/app/src/main/java/com/example/conal/soundrecord/ResultsActivity.java
+++ SoundRecord/app/src/main/java/com/example/conal/soundrecord/ResultsActivity.java
@@ -1,57 +1,108 @@
 package com.example.conal.soundrecord;
 
 import android.content.Intent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import static com.example.conal.soundrecord.HomeActivity.MyPREFERENCES;
+
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
 
+    Button btnNextDrop, btnFinish, btnRedo;
+    SharedPreferences sharedPreferences;
+    private boolean mapNeeded;
+
+    private SeekBar sb;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final TextView height1 = (TextView) findViewById(R.id.height1);
 
+        intent = getIntent();
+
+        Location loc = intent.getParcelableExtra(ProcessingActivity.LOCATION);
+        height1.setText(loc.getHeights().get(0).toString().substring(0, 4));
+
+        btnNextDrop = this.findViewById(R.id.next_drop_btn);
+        btnFinish = this.findViewById(R.id.finish_btn); // End the test early
+        btnRedo = this.findViewById(R.id.redo_btn);
+
+        sb = (SeekBar) findViewById(R.id.seekBar);
+
+        /** Doesn't allow the user to change the progress but still displays the marker on the seekbar**/
+        sb.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0; // Amount of progress changed
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress; // Used to set progress from both a user and the program
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.i("seekbar", "Progress is " + progressChanged);
+            }
+        });
+
+        sb.setProgress(50);
+
+
+        btnNextDrop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+                mapNeeded = sharedpreferences.getBoolean("mapNeeded", false);
+
+                if (mapNeeded) {
+                    openMapsActivity();
+                } else {
+                    openRecordingsPage();
+                }
+
+            }
+        });
+    }
+
+    public void openMapsActivity() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
 
     }
 
-
-    public void openHomePage(){
-        Intent intent = new Intent(this, HomeActivity.class);
+    public void openRecordingsPage() {
+        Intent intent = new Intent(this, RecordingActivity.class);
         startActivity(intent);
     }
 
-
-
-    //temporary way to get to the result page
-    public void openResultsPage(){
-        Intent intent = new Intent(this, ResultsActivity.class);
-        startActivity(intent);
-    }
-
-    //dropdown menu
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        if(item.getItemId() == R.id.home) {
-            openHomePage();
-        }
-      else if(item.getItemId() == R.id.results) {
-            openResultsPage();
-        }else if(item.getItemId() == R.id.my_account){
-            Toast.makeText(this, "This will be My Account page", Toast.LENGTH_SHORT).show();
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
 }
