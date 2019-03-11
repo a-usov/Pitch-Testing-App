f7feae2a2d02a1404e1b22b672975ced6d469994
Conall Clements <2197397c@student.gla.ac.uk>
2019-03-11 14:49:07 +0000

    Finished Junit tests for PitchTest - PitchTestTest.java

--- SoundRecord/app/src/test/java/com/example/conal/soundrecord/PitchTestTest.java
+++ SoundRecord/app/src/test/java/com/example/conal/soundrecord/PitchTestTest.java
@@ -6,25 +6,32 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.List;
+import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PitchTestTest {
 
+    PitchTest pitchTest;
     Location location1;
     Location location2;
     List<Location> locations;
     LatLng latlng;
-
-
+    Result result;
+    Result result2;
 
     @Before
     public void setUp() throws Exception {
-        latlng = new LatLng(55.8642, -4.2518 );
-        location1 = new Location(latlng);
-        locations.add(location1);
-
+        //Sets up a new
+        pitchTest = new PitchTest();
+        location1 = new Location(new LatLng(55.8642, -4.2518 ));
+        result = new Result(0,0,0);
+        result.setBounceHeight(64.2);
+        result2 = new Result(0,0,0);
+        result2.setBounceHeight(80.4);
     }
 
     @After
@@ -33,33 +40,56 @@ public class PitchTestTest {
 
     @Test
     public void checkAddLocationMethod() {
-        locations.add(location1);
-
+        for (int i = 0; i < 4; i++) {
+            location1.addResult(result);
+        }
+        pitchTest.addLocation(location1);
+        assertEquals(location1, pitchTest.getLocation(0));
     }
 
     @Test
     public void checkIsEmptyMethod() {
-        System.out.println(locations.isEmpty());
-        assertTrue(false == locations.isEmpty());
+        assertTrue(pitchTest.isEmpty());
     }
 
     @Test
-    public void getNumDone() {
+    public void checkGetNumDone() {
+        pitchTest.addLocation(location2);
+        pitchTest.incrementNumDone();
+        assertEquals(1, pitchTest.getNumDone());
     }
 
     @Test
-    public void incrementNumDone() {
+    public void checkIncrementNumDone() {
+        pitchTest.incrementNumDone();
+        assertEquals(1,pitchTest.getNumDone());
     }
 
     @Test
-    public void increaseLocNumDone() {
+    public void checkIncreaseLocNumDone() {
+        for (int i = 0; i < 4; i++) {
+            location1.addResult(result);
+            location1.incrementNumDone();
+        }
+        pitchTest.addLocation(location1);
+        pitchTest.incrementNumDone();
+        pitchTest.increaseLocNumDone(0);
+        assertEquals(5, pitchTest.getLocation(0).getNumDone());
     }
 
     @Test
-    public void getLocations() {
+    public void checkGetLocations() {
+        pitchTest.addLocation(location1);
+        pitchTest.addLocation(location2);
+        ArrayList<Location> locs = new ArrayList<Location>();
+        locs.add(location1);
+        locs.add(location2);
+        assertEquals(locs, pitchTest.getLocations());
     }
 
     @Test
-    public void getLocation() {
+    public void checkGetLocation() {
+        pitchTest.addLocation(location2);
+        assertEquals(location2, pitchTest.getLocation(0));
     }
 }
