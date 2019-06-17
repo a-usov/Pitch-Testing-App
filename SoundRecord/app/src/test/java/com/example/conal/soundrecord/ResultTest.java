e1dfb7768715f91e40df3addf8bf662e1eb0fc91
Artem Usov <artem.o.u@gmail.com>
2019-06-17 21:04:33 +0100

    added author, license header to all files

--- SoundRecord/app/src/test/java/com/example/conal/soundrecord/ResultTest.java
+++ SoundRecord/app/src/test/java/com/example/conal/soundrecord/ResultTest.java
@@ -1,3 +1,7 @@
+// AUTHORS - This code was written by Artem Usov, 2296905u@student.gla.ac.uk, Olga Jodelka 2266755K@student.gla.ac.uk
+// Kara Newlands 2264457n@student.gla.ac.uk, Conall Clements 2197397c@student.gla.ac.uk
+// It is licensed under the GNU General Public License version 3, 29 June 2007 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+
 package com.example.conal.soundrecord;
 
 import org.junit.Before;
